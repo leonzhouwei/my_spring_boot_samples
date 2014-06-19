ea32cb0c5e09b407ce06023b2cf13fd452df1988
zhouwei <leonzhouwei@gmail.com>
2014-06-19 21:59:50 +0800

    add logging

--- spring_boot_0400_spring_security_web_jdbc/src/main/java/hello/controller/EchoController.java
+++ spring_boot_0400_spring_security_web_jdbc/src/main/java/hello/controller/EchoController.java
@@ -1,17 +1,20 @@
 package hello.controller;
 
-import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.PathVariable; 
-import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.web.servlet.ModelAndView;  
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
   
 @Controller
 @RequestMapping("/echo")
 public class EchoController {  
+	private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
 
     @RequestMapping("/{id}")  
     public ModelAndView echo(@PathVariable("id") String id) {  
-        System.out.println("oops: in EchoController.echo() now");
+    	logger.info("oops: in EchoController.echo() now");
         ModelAndView mav = new ModelAndView();  
         mav.setViewName("echo");  
         mav.addObject("message", "Hello " + id + " !");  
