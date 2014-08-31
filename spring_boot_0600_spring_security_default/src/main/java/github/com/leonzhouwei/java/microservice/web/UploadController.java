ffa6ef22eeaa24057b5321dec908c67f4a3489f1
zhouwei <leonzhouwei@gmail.com>
2014-08-31 17:25:06 +0800

    my own html form can be submitted by HTTP POST method

---
+++ spring_boot_0600_spring_security_default/src/main/java/github/com/leonzhouwei/java/microservice/web/UploadController.java
@@ -0,0 +1,40 @@
+package github.com.leonzhouwei.java.microservice.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+  
+@Controller
+public class UploadController {
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    @Secured("ROLE_USER")
+    public ModelAndView upload(@RequestParam("key")String value) {  
+    	String ret = "\"" + value + "\" wrapped in POST from user " + getUserName();
+    	logger.debug(ret);
+        ModelAndView mav = new ModelAndView();  
+        mav.setViewName("upload");  
+        mav.addObject("message", ret);  
+        return mav;  
+    }
+    
+    private String getUserName() {
+    	String username = null;
+    	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	if (principal instanceof UserDetails) {
+    		username = ((UserDetails)principal).getUsername();
+    	} else {
+    		username = principal.toString();
+    	}
+    	return username;
+    }
+
+}  
